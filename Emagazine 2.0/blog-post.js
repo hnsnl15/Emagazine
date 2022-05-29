688c1b8403c4e68b6cc822f55e3b341f13eeefd6
henson <hensonlao@gmail.com>
2022-05-29 19:45:58 +0800

    Final update for this project...for now.

--- Emagazine 2.0/blog-post.js
+++
@@ -1,9 +0,0 @@
-const blogNav = document.querySelector('.blog-nav-text');
-
-if (innerWidth > 850) {
-window.addEventListener('scroll', function(){
-    blogNav.classList.toggle('sticky-dark', window.scrollY > 0);
-    blogNav.classList.toggle('blog-nav-text-white', window.scrollY > 0);
-    blogNav.classList.toggle('blog-nav-text2', window.scrollY > 0)
-})
-}
--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-var menuBtn = document.querySelector('.menu-btn');
-var navLinks = document.querySelector('.nav-links');
-var logo = document.querySelector('.logo');
-var logoo = document.querySelector('a');
-var nav = document.querySelector('.navbar');
-var blackLinks = document.querySelectorAll(".links");
-var link1 = document.querySelectorAll('a')[0];
-var link2 = document.querySelectorAll('a')[1];
-var link3 = document.querySelectorAll('a')[2];
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelector('.navbar');
+const navMobile = document.querySelector('.navbar-mobile');
+const blackLogo = document.querySelector('#logo');
+const scrolledNav = document.querySelector('.nav');
+let blackMenu1 = document.querySelectorAll('.navbar-links')[0];
+let blackMenu2 = document.querySelectorAll('.navbar-links')[1];
+let blackMenu3 = document.querySelectorAll('.navbar-links')[2];
 
-menuBtn.addEventListener('click', function() {
-    navLinks.classList.toggle('mobile-menu');
-    logo.classList.toggle('logo2')
-})
+function mobile() {
+    if (innerWidth < 850) {
+        menuBtn.addEventListener('click', function(){
+            navLinks.classList.toggle('mobile-menu');
+            menuBtn.classList.toggle('menu-btn-no-bg');
+            blackLogo.classList.toggle('blackItems');
+            blackMenu1.classList.toggle('blackItems');
+            blackMenu2.classList.toggle('blackItems');
+            blackMenu3.classList.toggle('blackItems');
+        });
+        window.addEventListener('scroll', function(){
+            navMobile.classList.toggle('scrollUp', window.scrollY > 100);
+        })
+    }
+    else {
+        window.addEventListener('scroll', function(){
+            nav.classList.toggle('sticky', window.scrollY > 500);
+            blackLogo.classList.toggle('blackItems', window.scrollY > 500);
+            blackMenu1.classList.toggle('blackItems', window.scrollY > 500);
+            blackMenu2.classList.toggle('blackItems', window.scrollY > 500);
+            blackMenu3.classList.toggle('blackItems', window.scrollY > 500);
+        });
+    }
+}
 
-window.addEventListener('scroll', function() {
-    nav.classList.toggle('sticky', window.scrollY > 0);
-    logoo.classList.toggle('logo2', window.scrollY > 0);
-    logoo.classList.toggle('blackk', window.scrollY > 0);
-    blackLinks[0].classList.toggle('blackk', window.scrollY > 0);
-    blackLinks[1].classList.toggle('blackk', window.scrollY > 0);
-    blackLinks[2].classList.toggle('blackk', window.scrollY > 0);
-    blackLinks[3].classList.toggle('blackk', window.scrollY > 0);
-
-})
+mobile();
